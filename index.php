f46efc39bfc16180fe92c5f493a20b4a0ac360fb
COMLAB <mkmartinez23-0347@cca.edu.ph>
2025-02-17 18:42:54 +0800

    index.php
    java

--- index.php
+++ index.php
@@ -1,44 +1,11 @@
-<html> 
-	 <head> 
-		<title>Javascript Form</title>	
-	</head>
-	<body style="background-color: blue; color:#FFF;">
-		 <h1> Student Record </h1>
-		
-		<table id ="student_table">
-		</table>
-		
-		<script>
-			<!-- GET TABLE ELEMENT-->
-			const studentTable = document
-				.getElementById("student_table");
-				
-			<!-- GET ROW-->
-			const row1 = studentTable.insertRow();
-			
-			<!-- CREATE 2 COLUMNS (CELL) -->
-			const row1col1 = row1.insertCell();
-			const row1col2 = row1.insertCell();
-			
-			row1col1.innerHTML = "LAST NAME";
-			row1col2.innerHTML = "FIRST NAME";
-			
-			const row2 = studentTable.insertRow();
-			const row1col1 =row2.insertCell();
-			const row1col2 =row2.insertCell();
-			
-			const inputLastName = document.createElement("input);
-			inputFirstName.type="text";
-			inputFirstName.placeholder="Insert First Name"
-			
-			row2col2.append(inputFirstname);
-			
-			const row3 = studentTable.insertRow();
-			const row3col1= row3.insertCell();
-			row3col1.colspan="2";
-			row3col1.innerHTML="TANGINAMO ASIONG";
-			
-				
-		  </script>
-	 </body>
-</html>
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
+?>
+Something is wrong with the XAMPP installation :-(
