9687c3e5fcfa5907267ad1965004dd2e3ce39616
COMLAB <mkmartinez23-0347@cca.edu.ph>
2025-02-17 18:56:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,55 @@
-<html> 
-	 <head> 
-		<title>Javascript Form</title>	
+<html>
+	<head>
+		<title>Javascript Form</title>
 	</head>
-	<body style="background-color: blue; color:#FFF;">
-		 <h1> Student Record </h1>
-		
-		<table id ="student_table">
-		</table>
-		
-		<script>
-			<!-- GET TABLE ELEMENT-->
-			const studentTable = document
-				.getElementById("student_table");
-				
-			<!-- GET ROW-->
+	<body>
+	<h1>Student Record</h1>
+	<table id="student_table" border ="1">
+	</table>
+
+			<script>
+			const studentTable = document.getElementById("student_table");
+			console.log(studentTable);
+
 			const row1 = studentTable.insertRow();
-			
-			<!-- CREATE 2 COLUMNS (CELL) -->
-			const row1col1 = row1.insertCell();
-			const row1col2 = row1.insertCell();
-			
+			const row1col1= row1.insertCell();
+			const row1col2=row1.insertCell();
+
 			row1col1.innerHTML = "LAST NAME";
-			row1col2.innerHTML = "FIRST NAME";
-			
+			row1col2.innerHTML = " FIRST NAME";
+
+
 			const row2 = studentTable.insertRow();
-			const row1col1 =row2.insertCell();
-			const row1col2 =row2.insertCell();
-			
-			const inputLastName = document.createElement("input);
+			const row2col1= row2.insertCell();
+			const row2col2=row2.insertCell();
+
+			const inputLastName = document.createElement("input");
+			inputLastName.type="text";
+			inputLastName.placeholder="Insert Last Name"
+
+			row2col1.append(inputLastName);
+
+			const inputFirstName = document.createElement("input");
 			inputFirstName.type="text";
 			inputFirstName.placeholder="Insert First Name"
-			
-			row2col2.append(inputFirstname);
-			
+
+			row2col2.append(inputFirstName);
+
 			const row3 = studentTable.insertRow();
 			const row3col1= row3.insertCell();
 			row3col1.colspan="2";
-			row3col1.innerHTML="TANGINAMO ASIONG";
-			
-				
-		  </script>
-	 </body>
-</html>
+
+			const inputSubmit = document.createElement("input");
+			inputSubmit.type="submit";
+			inputSubmit.value="Create Record";
+
+			row3col1.append(inputSubmit);
+
+			const studentForm = document.createElement("form");
+			studentForm.append=(studentTable);
+
+			body = document.getElementsByTagName('body')[0];
+			body.append(studentForm);
+	   </script>
+	</body>
+</html
