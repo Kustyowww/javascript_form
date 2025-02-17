a981b2228d98d37190e9a08df8868950beeb636b
COMLAB <mkmartinez23-0347@cca.edu.ph>
2025-02-17 19:43:14 +0800

    CREATE PHP
    PHP CREATE

--- create.php
+++ create.php
@@ -1,5 +1,53 @@
-<?php	
-	echo "Hello World";
-	var_dump($_POST);
+<?php
+
+echo"Hello World";
+var_dump($_POST);
+$lastname = $_POST['last_name'];
+$firstname = $_POST['first_name'];
+
+?>
+
+<br>
+<br>
+<br>
+<br>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database ="lorraine";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql = "INSERT INTO students (id,last_name, first_name)
+VALUES ('','$lastname','$firstname')";
+
+if ($conn->query($sql) === TRUE) {
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+echo"<br>";
+
+
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
 ?>
